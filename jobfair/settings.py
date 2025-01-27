ff343ea2a09e0b4812cb457b63b0bcc54d8213a5
YussofWaleed <yussofwaleed@gmail.com>
2025-01-27 15:39:39 +0200

    Update STATIC_ROOT path in settings to use 'staticfiles' directory

--- jobfair/settings.py
+++ jobfair/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Example path
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Example path
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 
