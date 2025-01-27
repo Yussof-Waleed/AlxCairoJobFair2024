2d27c6df77e826ce1909326941ad27aaa05938a8
YussofWaleed <yussofwaleed@gmail.com>
2025-01-27 08:33:18 +0200

    Add project directory to Python path in WSGI configuration

--- jobfair/wsgi.py
+++ jobfair/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Add the project directory to the Python path
+sys.path.append('/home/spyce/AlxCairoJobFair2024')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobfair.settings')
 
